4784984cb762932ec3dc6ded85f298752b170e37
Lilyo923 <hippolyte.duhemnelson@gmail.com>
2025-06-09 16:29:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -61,3 +61,7 @@ let tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 let firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+function goToNextStep() {
+  document.getElementById("video-screen").classList.add("hidden");
+  document.getElementById("next-step").classList.remove("hidden");
+}
